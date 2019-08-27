a6527785e8d2a34822eb5ee1109a3f80e840edaf
Bahay Gulle Bilgi and Zubeyde Sisman <bahay@>
2019-08-27 16:04:31 -0700

    Add constructor and prototype.

---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+// Business Logic for AddressBook ---------
+function Price() {
+  this.tickets = [];
+  //this.currentId = 0
+}
+//
+// AddressBook.prototype.addContact = function(contact) {
+//   contact.id = this.assignId();
+//   this.contacts.push(contact);
+// }
+
+Price.prototype.calculateTicket = function() {
+  for (var i=0; i<this.tickets.length; i++) {
+    if (this.tickets[i] === ) {
+
+        return this.tickets[i];
+      }
+    }
+  };
+  return false;
+}
+
+
+
+
+// Business Logic for Contacts ---------
+function Ticket(movieName, matinee, age) {
+  this.movieName = movieName,
+  this.matinee = matinee,
+  this.age = age
+}
