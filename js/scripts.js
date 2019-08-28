74e97df10fccde797844ea88d035d38118094c0a
Student <example@example.com>
2019-08-27 17:19:57 -0700

    Rewrite index.html and scripts.js.Work on progress

--- js/scripts.js
+++ js/scripts.js
@@ -3,29 +3,47 @@ function Price() {
   this.tickets = [];
   //this.currentId = 0
 }
-//
-// AddressBook.prototype.addContact = function(contact) {
-//   contact.id = this.assignId();
-//   this.contacts.push(contact);
-// }
 
-Price.prototype.calculateTicket = function() {
+Price.prototype.findMovie = function(movie) {
   for (var i=0; i<this.tickets.length; i++) {
-    if (this.tickets[i] === ) {
-
-        return this.tickets[i];
+    if (this.tickets[i].movieName === $("#movieName").val()){
+      return this.tickets[i];
       }
     }
-  };
-  return false;
-}
+  }
+
+Price.prototype.findmovieTime = function(movietime) {
+    for (var i=0; i<this.tickets.length; i++) {
+      if (this.tickets[i].movieTime === $("#movieTime").val()){
+        return this.tickets[i];
+        }
+      }
+    }
+
+ Price.prototype.findage = function(age) {
+    for (var i=0; i<this.tickets.length; i++) {
+      if (this.tickets[i].age === $("#age").val()){
+        return this.tickets[i];
+        }
+      }
+    }
 
 
 
 
-// Business Logic for Contacts ---------
-function Ticket(movieName, matinee, age) {
+// Business Logic for ticket ---------
+function Ticket(movieName, movietime, age) {
   this.movieName = movieName,
-  this.matinee = matinee,
+  this.movietime = movietime,
   this.age = age
 }
+// User Interface Logic ---------
+
+function displayticketPrice(priceToDisplay) {
+  var price = $("#result-price");
+  var htmlForPriceInfo = "";
+  priceToDisplay.Ticket.forEach(function(price) {
+  htmlForPriceeInfo += "<li id=" + place.id + ">" + place.location + " " + place.landmark + "</li>";
+  });
+  placesList.html(htmlForPriceInfo);
+};
