e1fd085e8c6bd7beec013a4d6a7043545044d8cf
Student <example@example.com>
2019-08-29 10:00:54 -0700

    Rewrite if statement to calculate ticket price right way.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,25 @@
 // Business Logic for Ticket -------
-    function Ticket(movieName, movieTime,age) {
-    this.movieName = movieName,
-    this.movieTime = movieTime,
-     this.age = age,
-     this.price = 20
+function Ticket(movieName, movieTime,age) {
+  this.movieName = movieName,
+  this.movieTime = movieTime,
+  this.age = age,
+  this.price = 20
 }
+// var newTicket = new Ticket(inputtedMovieName, inputtedMovieTime, inputtedAge);
+// newTicket.calculatePrice();
+// console.log(newTicket.price);
+
+
 Ticket.prototype.calculatePrice = function() {
-  if( this.movieName === "Spiderman" && (this.movieTime === "2pm" || this.movieTime === "8pm") && (this.age ==="kid" || this.age === "adult")){
-   this.price = 30;
- }else if (this.movieName === "spiderman" && this.movieTime === "10am" && this.age !== "senior") {
-   this.price =25;
- }else if (this.movieName === "spiderman"  && this.movieTime == "10am" && this.age === "senior") {
-   this.price = 20;
- }
+  if(this.movieName === "Spiderman" && (this.movieTime === "2pm" || this.movieTime === "8pm") && (this.age ==="kid" || this.age === "adult")){
+    this.price = 30;
+  }else if (this.movieName === "Spiderman" && (this.movieTime === "10am" && this.age !== "senior")) {
+    this.price = 25;
+  }else if (this.movieName !== "Spiderman"  && this.movieTime !== "10am" && this.age === "senior") {
+    this.price = 15;
+  }else if (this.movieName !== "Spiderman"  && this.movieTime == "10am" && this.age === "senior") {
+    this.price = 10;
+  }
 }
 //user interface logic-----
 $(document).ready(function() {
@@ -21,16 +28,15 @@ $(document).ready(function() {
     var inputtedMovieName = $("input:radio[name=movie]:checked").val();
     var inputtedMovieTime = $("input:radio[name=movieTime]:checked").val();
     var inputtedAge = $("input:radio[name=age]:checked").val();
-console.log(inputtedAge);
+    console.log(inputtedAge);
 
-
-  var newTicket = new Ticket(inputtedMovieName, inputtedMovieTime, inputtedAge);
+    var newTicket = new Ticket(inputtedMovieName, inputtedMovieTime, inputtedAge);
     newTicket.calculatePrice();
     console.log(newTicket.price);
 
 
- var finalPrice = newTicket.price;
-$(".finalprice").text(finalPrice);
+    var finalPrice = newTicket.price;
+    $(".finalprice").text(finalPrice);
     $("#result-price").show();
   });
 });
